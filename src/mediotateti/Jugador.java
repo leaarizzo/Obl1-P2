6dd3a7ac2ca04c3d704ab1aef0404f53aca06e38
Bruno Alfaro <brunoalfaro13@gmail.com>
2025-10-15 12:51:05 -0300

    Hola lea he hecho el menu y la funcion de registar jugador

--- src/mediotateti/Jugador.java
+++ src/mediotateti/Jugador.java
@@ -9,5 +9,30 @@ package mediotateti;
  * @author leaco
  */
 public class Jugador {
+    private String nombre;
+    private int edad;
+    private int ganadas;
+    private boolean invicto;
     
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.nombre = Nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.edad = Edad;
+    }
+    public Jugador(String nombre,int edad){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.ganadas=0;
+        this.invicto=true;
+}   
 }
